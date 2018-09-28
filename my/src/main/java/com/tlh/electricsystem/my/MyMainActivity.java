9becbbf024829dab044435ee2c28ba10723b1ca8
michael <cqupt.mike@qq.com>
2018-09-28 14:26:02 +0800

    项目模块化

---
+++ my/src/main/java/com/tlh/electricsystem/my/MyMainActivity.java
@@ -0,0 +1,13 @@
+package com.tlh.electricsystem.my;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_main);
+    }
+}
---
+++ operation/src/main/java/com/tlh/electricsystem/operation/OperationMainActivity.java
@@ -0,0 +1,13 @@
+package com.tlh.electricsystem.operation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperationMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operation_main);
+    }
+}
