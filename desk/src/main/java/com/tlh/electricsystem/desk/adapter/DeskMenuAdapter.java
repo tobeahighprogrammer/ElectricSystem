2f444251a399de6ea4387210901fdb7efaee84ff
michael <cqupt.mike@qq.com>
2018-09-29 11:56:57 +0800

    增加dataBinding配置

---
+++ desk/src/main/java/com/tlh/electricsystem/desk/adapter/DeskMenuAdapter.java
@@ -0,0 +1,55 @@
+package com.tlh.electricsystem.desk.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.tlh.electricsystem.desk.R;
+
+import java.util.List;
+
+public class DeskMenuAdapter extends RecyclerView.Adapter<DeskMenuAdapter.BindViewHolder> {
+
+    private Context context;
+    private List<String> dataList;
+
+    public DeskMenuAdapter(Context context,List<String> dataList){
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+
+    @NonNull
+    @Override
+    public BindViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ViewDataBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_desk_menu,parent,false);
+        return new BindViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BindViewHolder holder, int position) {
+        String name = dataList.get(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList == null ? 0 : dataList.size();
+    }
+
+    class BindViewHolder extends RecyclerView.ViewHolder{
+        private ViewDataBinding binding;
+        public BindViewHolder(ViewDataBinding binding){
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public ViewDataBinding getBinding() {
+            return binding;
+        }
+    }
+}
--- desk/src/main/java/com/tlh/electricsystem/desk/fragment/DeskFragment.java
+++ desk/src/main/java/com/tlh/electricsystem/desk/fragment/DeskFragment.java
@@ -1,4 +1,4 @@
-package com.tlh.electricsystem.desk;
+package com.tlh.electricsystem.desk.fragment;
 
 
 import android.os.Bundle;
@@ -7,8 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.tlh.electricsystem.desk.R;
+
 
 /**
+ * 工作台首页
  * A simple {@link Fragment} subclass.
  */
 public class DeskFragment extends Fragment {
@@ -20,9 +23,7 @@ public class DeskFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_desk, container, false);
     }
 
